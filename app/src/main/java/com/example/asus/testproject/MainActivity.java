21cde115fcb697c20569c642a42b485bd2c6adf1
Sergii Shevchenko <zoomx3m@gmail.com>
2016-02-18 12:08:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/asus/testproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.testproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Asus on 13.02.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
